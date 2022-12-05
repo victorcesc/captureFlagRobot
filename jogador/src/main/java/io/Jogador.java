e4b976acd2b03e6622d67ce41bab85e33c7d1940
victorcesc <victorcesconetto@bry.com.br>
2022-12-04 21:40:31 -0300

    subindo os dois containers com docker compose, porem com falhas

--- jogador/src/main/java/io/Jogador.java
+++ jogador/src/main/java/io/Jogador.java
@@ -60,7 +60,7 @@ public class Jogador {
   public static void main(String[] args) throws Exception {
     String user = "world";
     // Access a service running on the local machine on port 50051
-    String target = "localhost:50051";
+    String target = "172.18.0.2:50051";
     // Allow passing in the user and target strings as command line arguments
     if (args.length > 0) {
       if ("--help".equals(args[0])) {
